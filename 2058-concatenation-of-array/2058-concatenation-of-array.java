e6eb5fed52b9b422d78b1ee0f8b80e751671ed45
Ayush-Singh-16 <djayushisstar@gmail.com>
2025-08-13 10:56:04 +0530

    Time: 1 ms (96.38%), Space: 45.5 MB (30.44%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Solution {
+    public static int[] getConcatenation(int[] nums) {
+       int n=nums.length;
+       int[] ans=new int[2*n];
+       for(int i=0;i<n;i++){
+        ans[i]=nums[i];
+        ans[i+n]=nums[i];
+       }
+       return ans;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the length of athe array");
+        int[] nums={1,2,1};
+        System.out.print(Arrays.toString(nums));
+    }
+}
