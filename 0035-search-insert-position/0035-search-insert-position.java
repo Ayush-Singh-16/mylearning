67f69dde00406b6e70688796a3b9d1bdcc59a742
Ayush-Singh-16 <djayushisstar@gmail.com>
2025-09-08 22:13:23 +0530

    Time: 0 ms (100%), Space: 43.4 MB (19.04%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int start=0,end=nums.length-1;
-        if(nums[end]<target){
-            return end+1;
-        }
-        else{
+        
             while(start<=end){
                 int mid=start+(end-start)/2;
                 if(nums[mid]==target){
@@ -16,8 +13,8 @@ class Solution {
             else{
                 start=mid+1;
             }     
+        
         }
-    }
           return start;
     }
 }
