2c428698495e71aae2949c86fa243d1bf89d567f
Ayush-Singh-16 <djayushisstar@gmail.com>
2025-11-12 19:08:14 +0530

    Time: 1 ms (17.4%), Space: 43.1 MB (5.21%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toLowerCase(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if((int)s.charAt(i)<=90 && 65<=(int)s.charAt(i)){
+                sb.append((char)(s.charAt(i)+32));
+            }
+            else{
+                sb.append((char)(s.charAt(i)));
+            }
+        } 
+        return sb.toString();
+    }
+}
